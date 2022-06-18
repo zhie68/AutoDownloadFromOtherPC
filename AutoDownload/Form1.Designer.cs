88080408844d17593906d0d8c449894ab1006f70
Muhammad Fauji <88869085+zhie68@users.noreply.github.com>
2022-06-18 22:42:43 +0700

    Add files via upload

---
+++ AutoDownload/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace AutoDownload
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker4 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(441, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Download";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_KeyDown);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 11);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(162, 20);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker2.Location = new System.Drawing.Point(196, 12);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(145, 20);
+            this.dateTimePicker2.TabIndex = 2;
+            // 
+            // dateTimePicker3
+            // 
+            this.dateTimePicker3.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker3.Location = new System.Drawing.Point(12, 47);
+            this.dateTimePicker3.Name = "dateTimePicker3";
+            this.dateTimePicker3.ShowUpDown = true;
+            this.dateTimePicker3.Size = new System.Drawing.Size(162, 20);
+            this.dateTimePicker3.TabIndex = 3;
+            // 
+            // dateTimePicker4
+            // 
+            this.dateTimePicker4.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker4.Location = new System.Drawing.Point(196, 47);
+            this.dateTimePicker4.Name = "dateTimePicker4";
+            this.dateTimePicker4.ShowUpDown = true;
+            this.dateTimePicker4.Size = new System.Drawing.Size(145, 20);
+            this.dateTimePicker4.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(368, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Folder Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(441, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(441, 47);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(100, 21);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 86);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(329, 23);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "label2";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(360, 80);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 30);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Open";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(552, 115);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker4);
+            this.Controls.Add(this.dateTimePicker3);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Auto Download";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ AutoDownload/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Net;
+using System.IO;
+using System.Collections.Generic;
+using System.Threading;
+
+
+namespace AutoDownload
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        static extern IntPtr FindWindow(IntPtr classname, string title); // extern method: FindWindow
+
+        [DllImport("user32.dll")]
+        static extern void MoveWindow(IntPtr hwnd, int X, int Y,
+            int nWidth, int nHeight, bool rePaint); // extern method: MoveWindow
+
+        [DllImport("user32.dll")]
+        static extern bool GetWindowRect
+            (IntPtr hwnd, out Rectangle rect); // extern method: GetWindowRect
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            Dictionary<string, string> AuthorList = new Dictionary<string, string>();
+            //Dictionary comboSource = new Dictionary();
+            AuthorList.Add("1", "CB");
+            AuthorList.Add("2", "CS");
+            AuthorList.Add("3", "CS SCH");
+            AuthorList.Add("4", "DSS");
+            AuthorList.Add("5", "DS");
+            AuthorList.Add("6", "MRK");
+            AuthorList.Add("7", "INS");
+            AuthorList.Add("8", "CL");
+            comboBox1.DataSource = new BindingSource(AuthorList, null);
+            comboBox1.DisplayMember = "Value";
+            comboBox1.ValueMember = "Key";
+
+            dateTimePicker3.Format = DateTimePickerFormat.Custom;
+            dateTimePicker3.CustomFormat = "HH:mm:ss";
+
+            dateTimePicker4.Format = DateTimePickerFormat.Custom;
+            dateTimePicker4.CustomFormat = "HH:mm:ss";
+
+            label2.Size = new System.Drawing.Size(200, 30);
+            label2.AutoSize =  true;
+            
+            progressBar1.Minimum = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fname = textBox1.Text;
+            if (fname == "")
+            {
+                //MessageBox.Show("Please Input Folder Name");
+                int x = this.Width;
+                Point fx = this.Location;
+                FindAndMoveMsgBox(fx.X + x / 3, fx.Y, true, "Alert");
+                MessageBox.Show("Please Input Folder Name", "Alert");
+
+            }
+            else
+            {
+                //for get IP address from client
+                string selected = this.comboBox1.GetItemText(this.comboBox1.SelectedItem);
+                string iptarget = "";
+                switch (selected)
+                {
+                    case "CB":
+                        iptarget = @"\\172.21.86.150\d$\PICT\";
+                        break;
+                    case "CS":
+                        iptarget = @"\\172.21.86.47\d$\PICT\";
+                        break;
+                    case "CS SCH":
+                        iptarget = @"\\172.21.86.48\d$\PICT\";
+                        break;
+                    case "DSS":
+                        iptarget = @"\\172.21.86.50\d$\PICT\";
+                        break;
+                    case "DS":
+                        iptarget = @"\\172.21.86.44\d$\PICT\";
+                        break;
+                    case "MRK":
+                        iptarget = @"\\172.21.86.56\d$\PICT\";
+                        break;
+                    case "INS":
+                        iptarget = @"\\172.21.86.52\d$\PICT\";
+                        break;
+                    case "CL":
+                        iptarget = @"\\172.21.86.58\d$\System32\Windows\";
+                        break;
+                }
+                string dest = @"D:\" + fname + @"\";
+
+                // For get Date
+                string theDate1 = dateTimePicker1.Value.ToString("yyyy-MM-dd") + " " + dateTimePicker3.Value.ToString("HH:mm:ss");
+                string theDate2 = dateTimePicker2.Value.ToString("yyyy-MM-dd") + " " + dateTimePicker4.Value.ToString("HH:mm:ss");
+                DateTime dt = DateTime.Parse(theDate1);
+                DateTime dt2 = DateTime.Parse(theDate2);
+                TimeSpan rt = dt2 - dt;
+                int tothour = rt.Days * 24 * 60 * 60 + rt.Hours * 60 * 60 + rt.Minutes * 60 + rt.Seconds;
+                
+                string dx = "";
+                int tot =0;
+                if (tothour > 0)
+                {
+                    label2.Visible = true;
+                    progressBar1.Maximum = tothour;   
+                    Directory.CreateDirectory(dest);
+                    for (int i = 1; i <= tothour; i++)
+                    {
+                        DateTime d2 = dt.AddSeconds(i);
+                        string ssname = "Cap " + d2.ToString("yyyy-MM-dd HH-mm-ss") + ".png";
+                        string target = iptarget + ssname;
+                        if (File.Exists(target))
+                        {
+                            File.Copy(target, dest + ssname, true);
+                           
+                            label2.Text = "Copy ... "+ssname;
+                            label2.Refresh();
+                            tot++;
+                        }
+                        
+                        progressBar1.Value = i;
+
+
+                    }
+                    int x = this.Width;
+                    Point fx = this.Location;
+                    FindAndMoveMsgBox(fx.X + x / 3, fx.Y, true, "Alert");
+                    MessageBox.Show("Copy Done : " + tot + " File","Alert");
+                    //MessageBox.Show("Copy Done : "+ tot +" File");
+                }
+                else
+                {
+                    int x = this.Width;
+                    Point fx = this.Location;
+                    FindAndMoveMsgBox(fx.X + x / 3, fx.Y, true, "Alert");
+                    MessageBox.Show("Please Check Date and Time", "Alert");
+                }
+
+
+            }
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1.PerformClick();
+            }
+        }
+
+        private void button1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1.PerformClick();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string fname = textBox1.Text;
+           
+            if (fname == "")
+            {
+                int t = this.Width;
+                Point fx1 = this.Location;
+                FindAndMoveMsgBox(fx1.X + t / 3, fx1.Y, true, "Alert");
+                MessageBox.Show("Folder Not Found", "Alert");
+            }
+            else
+            {
+                string local = @"D:\" + fname + @"\";
+                Process.Start("explorer.exe", local);
+            }
+            //string c = @"C:\test\";
+            
+        }
+
+        void FindAndMoveMsgBox(int x, int y, bool repaint, string title)
+        {
+            Thread thr = new Thread(() => // create a new thread
+            {
+                IntPtr msgBox = IntPtr.Zero;
+                // while there's no MessageBox, FindWindow returns IntPtr.Zero
+                while ((msgBox = FindWindow(IntPtr.Zero, title)) == IntPtr.Zero) ;
+                // after the while loop, msgBox is the handle of your MessageBox
+                Rectangle r = new Rectangle();
+                GetWindowRect(msgBox, out r); // Gets the rectangle of the message box
+                MoveWindow(msgBox /* handle of the message box */, x, y,
+                   r.Width - r.X /* width of originally message box */,
+                   r.Height - r.Y /* height of originally message box */,
+                   repaint /* if true, the message box repaints */);
+            });
+            thr.Start(); // starts the thread
+        }
+    }
+}
